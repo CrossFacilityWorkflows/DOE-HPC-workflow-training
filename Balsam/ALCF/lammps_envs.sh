03e7ab454dc0c2d87f05aac74eca2880eba24cfb
Christine Simpson <csimpson@polaris-login-02.hsn.cm.polaris.alcf.anl.gov>
2023-04-11 13:36:00 -0500

    updated envs

--- Balsam/ALCF/lammps_envs.sh
+++ Balsam/ALCF/lammps_envs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export MPICH_GPU_SUPPORT_ENABLED=1
-export LAMMPS_PATH="/lus/eagle/projects/datascience/csimpson/lammps/my-lammps/lammps-3Nov2022/src"
+export LAMMPS_PATH="/soft/applications/lammps/lammps-3Nov2022/src/"
 PATH=$PATH:$LAMMPS_PATH
 
