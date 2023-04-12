b7dd0c30a5cb004dc5a259bfde199abafe75aba9
Sean Wilkinson <wilkinsonsr@ornl.gov>
2023-04-11 20:18:35 -0400

    Comments for run-workflow.py

--- FireWorks/OLCF/run-workflow.py
+++ FireWorks/OLCF/run-workflow.py
@@ -2,6 +2,10 @@
 #
 #-  run-workflow.py ~~
 #
+#-  Do not run this script manually, because it will fail when run on a login
+#   node. Instead, this script will be run by the LSF batch script:
+#       $ bsub batch-runner.lsf
+#
 
 import os
 from fireworks import LaunchPad
@@ -10,8 +14,7 @@ from fireworks.core.rocket_launcher import rapidfire
 # Set up and reset the LaunchPad using MongoDB URI string.
 launchpad = LaunchPad(host = os.getenv("MONGODB_URI"), uri_mode = True)
 
-# Launch workflow locally
+# Launch workflow "locally" -- this will run on a Summit batch node.
 print("Looking for workflows ...")
 rapidfire(launchpad)
 print("Done.")
-
